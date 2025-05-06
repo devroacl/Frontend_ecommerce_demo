5f2560d51e0d3fe087d0957247da2ed0cc214a33
ale <roa.alejandra02@gmail.com>
2025-05-05 23:26:24 -0400

    se modificaron archivos para ver si detecta bien las importaciones y rutas

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import path from 'path'; // Asegúrate de tener esto instalado
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'), // Debe apuntar a "./src"
     },
   },
-  base: '/', // Para Vercel
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-}); 
+});
